d1bc433a90111f8607a4192c7ae03bab73f1809c
Giang Nguyễn Văn <giangveal1998@gmail.com>
2022-06-09 17:30:41 +0700

    tests

--- src/pages/tutor/Add.js
+++ src/pages/tutor/Add.js
@@ -219,6 +219,9 @@ const Add = () => {
                             </Box>
                         </Box>
                     </Grid>
+                    <Box>
+                        giang
+                    </Box>
                     <Grid item xs={6}>
                         <Typography variant="h6" component="h2">
                             Công việc
